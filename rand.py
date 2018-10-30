fc56b011cb9a477209193810c89809a76e59dd51
lkb8xt <lkb8xt@virginia.edu>
2018-10-30 10:55:26 -0400

    Update rand.py

--- rand.py
+++ rand.py
@@ -9,13 +9,6 @@ import sseclient
 
 WIKI_STREAM_URL = 'https://stream.wikimedia.org/v2/stream/recentchange'
 
-# Since /dev/random is the closest thing to "true" randomness on a computer, and basically all other PRNGS use it, I thought I would have some fun
-# and use a live stream of recent wiki changes to emulate entropy and use as the input to a hash chain (similar to what dev/urandom does with /dev/random/'s
-# entropy pool).
-#  
-# I use the current epoc time, concatenated with a random num from /dev/urandom, as my initial seed, and then use each subsequent
-# hash as the new seed. 
-
 def generate_hash(seed, entropy):
     input = seed + entropy.encode('utf-8')
     return hashlib.sha256(input).digest()
